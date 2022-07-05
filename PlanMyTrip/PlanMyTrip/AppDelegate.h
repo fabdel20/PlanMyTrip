bf163f49d2a2e96d82e375747aac7bf29117dd01
Farida Abdelmoneum <fabdel20@fb.com>
2022-07-05 11:41:18 -0700

    set up

---
+++ PlanMyTrip/PlanMyTrip/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  PlanMyTrip
+//
+//  Created by Farida Abdelmoneum on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ PlanMyTrip/PlanMyTrip/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  PlanMyTrip
+//
+//  Created by Farida Abdelmoneum on 7/5/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
