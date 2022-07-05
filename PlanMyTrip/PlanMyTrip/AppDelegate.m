948feb5e38b9d6ffaa9472da4a6eaa51f732d149
Farida Abdelmoneum <fabdel20@fb.com>
2022-07-05 13:23:21 -0700

    connect to parse

--- PlanMyTrip/PlanMyTrip/AppDelegate.m
+++ PlanMyTrip/PlanMyTrip/AppDelegate.m
@@ -6,6 +6,7 @@
 //
 
 #import "AppDelegate.h"
+#import "Parse/Parse.h"
 
 @interface AppDelegate ()
 
@@ -13,9 +14,17 @@
 
 @implementation AppDelegate
 
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(  NSDictionary *)launchOptions {
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    ParseClientConfiguration *config = [ParseClientConfiguration  configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
+
+        configuration.applicationId = @"wxzqERdPjckzLAmjjt7Css9b5oJIqgxAjbPkMweq"; // <- UPDATE
+        configuration.clientKey = @"2ZxV04zVg502WTGAumrXE9jId9NDMy5uguUCnPVT"; // <- UPDATE
+        configuration.server = @"https://parseapi.back4app.com";
+    }];
+
+    [Parse initializeWithConfiguration:config];
+
     return YES;
 }
 
