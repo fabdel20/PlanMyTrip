f9fc5eec2e9235eb5c631f8c5fed1c72c21d2c3c
Farida Abdelmoneum <fabdel20@fb.com>
2022-07-07 12:16:37 -0700

    update

--- PlanMyTrip/PlanMyTrip/SelectServicesViewController.m
+++ PlanMyTrip/PlanMyTrip/SelectServicesViewController.m
@@ -8,6 +8,16 @@
 #import "SelectServicesViewController.h"
 
 @interface SelectServicesViewController ()
+-(void)changeColorOfButton:(UIButton *) Button checkState:(BOOL)state;
+@property (nonatomic) BOOL isHotelSelected;
+@property (nonatomic) BOOL isFlightSelected;
+@property (nonatomic) BOOL isCarSelected;
+@property (strong, nonatomic) IBOutlet UIButton *carsButton;
+@property (strong, nonatomic) IBOutlet UIButton *flightsButton;
+@property (strong, nonatomic) IBOutlet UIButton *hotelsButton;
+- (IBAction)carsButtonTapped:(id)sender;
+- (IBAction)flightsButtonTapped:(id)sender;
+- (IBAction)hotelsButtonTapped:(id)sender;
 
 @end
 
@@ -16,6 +26,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _hotelsButton.backgroundColor = [UIColor whiteColor];
+    _flightsButton.backgroundColor = [UIColor whiteColor];
+    _carsButton.backgroundColor = [UIColor whiteColor];
+}
+
+-(void)changeColorOfButton:(UIButton *) Button checkState :(BOOL)state{
+    Button.backgroundColor = Button ? [UIColor redColor] : [UIColor whiteColor];
+    state = !state;
 }
 
 /*
@@ -28,4 +46,34 @@
 }
 */
 
+- (IBAction)hotelsButtonTapped:(id)sender {
+    //[changeColorOfButton Button:_hotelsButton checkState: _isHotelSelected];
+    if(!_isHotelSelected){
+        _hotelsButton.backgroundColor = [UIColor redColor];
+        _isHotelSelected = YES;
+    } else if(_isHotelSelected){
+        _hotelsButton.backgroundColor = [UIColor whiteColor];
+        _isHotelSelected = NO;
+    }
+}
+
+- (IBAction)flightsButtonTapped:(id)sender {
+    if(!_isFlightSelected){
+        _flightsButton.backgroundColor = [UIColor redColor];
+        _isFlightSelected = YES;
+    } else if(_isFlightSelected){
+        _flightsButton.backgroundColor = [UIColor whiteColor];
+        _isFlightSelected = NO;
+    }
+}
+
+- (IBAction)carsButtonTapped:(id)sender {
+    if(!_isCarSelected){
+        _carsButton.backgroundColor = [UIColor redColor];
+        _isCarSelected = YES;
+    } else if(_isCarSelected){
+        _carsButton.backgroundColor = [UIColor whiteColor];
+        _isCarSelected = NO;
+    }
+}
 @end
