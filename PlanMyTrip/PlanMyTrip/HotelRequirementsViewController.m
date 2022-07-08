0656a8e069c443dfbb6873b822e22c1bf0daa573
Farida Abdelmoneum <fabdel20@fb.com>
2022-07-08 16:04:54 -0700

    book your hotel page

---
+++ PlanMyTrip/PlanMyTrip/HotelRequirementsViewController.m
@@ -0,0 +1,31 @@
+//
+//  HotelRequirementsViewController.m
+//  Pods
+//
+//  Created by Farida Abdelmoneum on 7/8/22.
+//
+
+#import "HotelRequirementsViewController.h"
+
+@interface HotelRequirementsViewController ()
+
+@end
+
+@implementation HotelRequirementsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
